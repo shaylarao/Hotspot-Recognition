a5094614ca23367163e99c0152e9600051b3e481
Sr22 <shayla.rao@gmail.com>
2020-12-07 16:48:10 -0800

    Added comments

--- fire.py
+++ fire.py
@@ -1,10 +1,17 @@
+# Shayla Rao
+# Color detection of thermal image of a fire using OpenCV
+
+# Imported cv2 and numpy
 import cv2
 import numpy as np
 
+# Getting thermal image
 img = cv2.imread( 'C:/Users/Shayla/Documents/Capstone_Class/image_proc/fire.jpg')
-#window_name = 'image'
+
+# Converting image colors from BGR format to HSV format
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
+# Defining lower and upper HSV ranges of colors
 lower_red = np.array([161,155,84])
 upper_red = np.array([179,255,255])
 
@@ -17,27 +24,23 @@ upper_orange = np.array([18,255,255])
 lower_yellow = np.array([20,100,100])
 upper_yellow = np.array([30,255,255])
 
+# Defining masks for colors using lower and upper ranges
 red_mask = cv2.inRange(hsv, lower_red, upper_red)
 red2_mask = cv2.inRange(hsv, lower_red2, upper_red2)
 orange_mask = cv2.inRange(hsv, lower_orange, upper_orange)
 yellow_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
 
+# Creating a mask with all colors extracted
 mask = red_mask+red2_mask+orange_mask+yellow_mask
-#mask = np.stack((combo_mask,)*2, axis=-1)
 
-#colored
+# Creating mask containing just the colors of the fire
 colored_mask = cv2.bitwise_or(img, img, mask=mask)
-#colored_mask = colored_mask[0:rows, 0:cols]
 
-output = red_mask+red2_mask+orange_mask+yellow_mask
-
-
+# Outputting original image, image with white mask, and, image with colored mask
 cv2.imshow('image', img)
 cv2.imshow('mask', mask)
 cv2.imshow('colored_mask', colored_mask)
 
-
+# Handles windowing events and destroys all windows created
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-#multiple colors: https://stackoverflow.com/questions/24892615/tracking-multiple-objects-by-color-opencv-2-x
